dc6e7d1e935f361c9b75cc9962ae9d2491fecf38
yusan25c <11522987+yusan25c@users.noreply.github.com>
2025-11-15 19:44:46 +0900

    modeの修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,20 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/log2mermaid/" : "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isGithub = mode === "github";
+
+  return {
+    base: isGithub ? "/log2mermaid/" : "/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
